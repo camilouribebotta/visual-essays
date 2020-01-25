811b709cac6e414362d2129cf4d0275b85c90e38
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-01-25 10:58:48 -0500

    mobile testing

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,7 +2,8 @@
   <v-layout>
     <v-flex>
       <div>url: {{ url }}</div>
-      <div>resp: {{ resp }}</div>
+      <div>resp1: {{ resp1 }}</div>
+      <div>resp2: {{ resp2 }}</div>
       <div>md: {{ md }}</div>
       <div ref="index" v-html="html"/>
     </v-flex>
@@ -11,23 +12,25 @@
 
 <script>
   import axios from 'axios'
-  import marked from 'marked'
   import { parseUrl } from '../utils'
 
   export default {
     name: 'index',
     data: () => ({
       url: undefined,
-      resp: undefined,
+      resp1: undefined,
+      resp2: undefined,
       md: undefined,
       html: undefined
     }),
     mounted() {
       this.url = `${process.env.app_md_endpoint}/${this.$options.name}.md`
+
+      axios.get(this.url).then(resp => this.resp1 = JSON.stringify(resp))
+
       this.$axios.get(this.url)
       .then((resp) => {
-        console.log(resp)
-        this.resp = JSON.stringify(resp)
+        this.resp2 = JSON.stringify(resp)
         this.md = resp.data
         this.html = this.$marked(this.md)
         this.$nextTick(() => {
