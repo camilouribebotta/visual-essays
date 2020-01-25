7f056b8734607def84faf97ff55a7aa53b3b2dc4
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-01-25 11:11:58 -0500

    Mobile testing

--- client/pages/about.vue
+++ client/pages/about.vue
@@ -1,23 +1,39 @@
 <template>
   <v-layout>
     <v-flex>
-      <div>src: {{src}}</div>
-      <div v-html="html"/>
+      <div>url: {{ url }}</div>
+      <div>resp1: {{ resp1 }}</div>
+      <div>resp2: {{ resp2 }}</div>
+      <div>md: {{ md }}</div>
+      <div ref="index" v-html="html"/>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+  import axios from 'axios'
+  import { parseUrl } from '../utils'
+
   export default {
     name: 'about',
     data: () => ({
-      html: undefined,
-      src: undefined
+      url: undefined,
+      resp1: undefined,
+      resp2: undefined,
+      md: undefined,
+      html: undefined
     }),
     mounted() {
-      this.src = `${process.env.app_md_endpoint}/${this.$options.name}.md`
-      this.$axios.get(this.src)
-      .then(resp => this.html = this.$marked(resp.data))
+      this.url = `${process.env.app_md_endpoint}/${this.$options.name}.md`
+
+      axios.get(this.url).then(resp => this.resp1 = JSON.stringify(resp))
+
+      this.$axios.get(this.url)
+      .then((resp) => {
+        this.resp2 = JSON.stringify(resp)
+        this.md = resp.data
+        this.html = this.$marked(this.md)
+      })
     }
   }
 </script>
