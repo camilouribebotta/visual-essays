8b2d6ea782c5162e960d9f57a5058d4b49f0dd5f
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-02-18 16:26:49 -0500

    Added custom component for http loading test

---
+++ docs/components/EntityViewer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>{{$options.name}}: items={{items.length}}
+</template>
+
+<script>
+  export default {
+    name: 'CustomEntityViewer',
+    props: {
+      items: { type: Array, default: () => ([]) }
+    }
+  }
+</script>
