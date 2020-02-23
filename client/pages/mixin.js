7347a1c4fb3a62ef39a11f6a548b6c3b7ac1292b
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-02-23 08:22:07 -0500

    GH pages test build

--- client/pages/mixin.js
+++ client/pages/mixin.js
@@ -59,7 +59,7 @@ export default {
           this.$refs[this.$options.name].querySelectorAll('a').forEach((link) => {
             if (link.href) {
               const parsedUrl = parseUrl(link.href)
-              console.log(`parsedUrl.origin=${parsedUrl.origin} baseUrl=${this.baseUrl} window.location.origin=${window.location.origin} window.location.hostname=${window.location.hostname}`)
+              console.log(`parsedUrl.origin=${parsedUrl.origin} baseUrl=${this.baseUrl} window.location.origin=${window.location.origin} window.location.hostname=${window.location.hostname} markup=${this.markup}`)
               if ((this.baseUrl.indexOf(parsedUrl.origin) === 0 || window.location.origin === parsedUrl.origin || window.location.hostname === 'localhost') &&
                    link.href.indexOf('#') === -1) {
                 // if (parsedUrl.pathname.slice(0, 6) === '/wiki/') {
--- client/utils/index.js
+++ client/utils/index.js
@@ -1,14 +1,14 @@
 export function parseUrl (href) {
-    /* eslint-disable no-useless-escape */
-    const match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)(\/[^?#]*)(\?[^#]*|)(#.*|)$/)
-    return match && {
-      protocol: match[1],
-      host: match[2],
-      hostname: match[3],
-      origin: `${match[1]}://${match[2]}`,
-      port: match[4],
-      pathname: match[5],
-      search: match[6],
-      hash: match[7]
-    }
+  /* eslint-disable no-useless-escape */
+  const match = href.match(/^(https?)\:\/\/(([^:\/?#]*)(?:\:([0-9]+))?)(\/[^?#]*)(\?[^#]*|)(#.*|)$/)
+  return match && {
+    protocol: match[1],
+    host: match[2],
+    hostname: match[3],
+    origin: `${match[1]}://${match[2]}`,
+    port: match[4],
+    pathname: match[5],
+    search: match[6],
+    hash: match[7]
   }
+}
