83e6ff00487c2be9b718a8b9ab502d0e1e0f9380
jkeup <jessica.keup@ithaka.org>
2020-04-07 12:56:53 -0400

    some basic changes to the vtl view

--- client-lib/src/layouts/VerticalLayout.vue
+++ client-lib/src/layouts/VerticalLayout.vue
@@ -1,12 +1,12 @@
 <template>
   <v-container>
     <v-row no-gutters v-if="layout === 'vtl'">
-      <v-col class="essay-pane"><essay/></v-col>
-      <v-col class="map-pane"><vertical-viewer/></v-col>
+      <v-col class="essay-pane vtl "><essay/></v-col>
+      <v-col class="map-pane vtl"><vertical-viewer/></v-col>
     </v-row>
     <v-row no-gutters v-else>
-      <v-col class="map-pane"><vertical-viewer/></v-col>
-      <v-col class="essay-pane"><essay/></v-col>
+      <v-col class="map-pane vtr"><vertical-viewer/></v-col>
+      <v-col class="essay-pane vtr"><essay/></v-col>
     </v-row>
   </v-container>
 </template>
@@ -30,9 +30,14 @@ export default {
     z-index: 10;
   }
 
-  .essay-pane {
+  .essay-pane.vtr  {
     z-index: 100;
     box-shadow: -5px 0px 10px 0px rgba(0,0,0,0.16);
+  }
+
+  .essay-pane.vtl  {
+    z-index: 100;
+    box-shadow: 5px 5px 10px 0px rgba(0,0,0,0.16);
   }
 
 </style>
