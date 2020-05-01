ec395931fd9c5bd78f9045dbcdf2b837feb5c932
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-05-01 08:29:58 -0400

    Initial working version of header scroll propagation fix

--- client-lib/src/main.js
+++ client-lib/src/main.js
@@ -33,7 +33,7 @@ import EntityViewer from './components/EntityViewer'
 import EntityInfobox from './components/EntityInfobox'
 import EntityInfoboxDialog from './components/EntityInfoboxDialog'
 
-const VERSION = '0.5.8'
+const VERSION = '0.5.9'
 
 console.log(`visual-essays js lib ${VERSION}`)
 
