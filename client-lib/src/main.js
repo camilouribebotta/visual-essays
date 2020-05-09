0dc352fcf6205439ef25dda638a5c8d8b0d17ba5
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-05-09 13:34:25 -0400

    New build with markdown styling changes

--- client-lib/src/main.js
+++ client-lib/src/main.js
@@ -33,7 +33,7 @@ import EntityViewer from './components/EntityViewer'
 import EntityInfobox from './components/EntityInfobox'
 import EntityInfoboxDialog from './components/EntityInfoboxDialog'
 
-const VERSION = '0.5.31'
+const VERSION = '0.5.32'
 
 console.log(`visual-essays js lib ${VERSION}`)
 
